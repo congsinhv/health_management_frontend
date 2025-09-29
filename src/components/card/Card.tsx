4911b148cb2080959088eff57ecb6bf946cedb16
TrucLam <httlam2004ntkt@gmail.com>
2025-09-29 07:53:43 +0700

    feat: add Gilroy font family and update styles
    - Introduced the Gilroy font family across the application, replacing the previous font settings.
    - Updated global styles to include font utilities for various weights of Gilroy.
    - Created new components for header and card with appropriate styling.
    - Added new SVG and JSON assets for logos.
    - Implemented a responsive hero section layout in the landing page.
    - Enhanced navigation with dynamic Lottie animation for chat feature.

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import styles from './Card.module.scss';
+
+interface CardProps {
+  title: string;
+  description: string;
+  isShowDescription: boolean;
+  isImage: boolean;
+}
+
+const Card = () => {
+  return <div></div>;
+};
+
+export default Card;
