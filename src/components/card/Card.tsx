976277210ac2378044339d0ca7c875b3d962d0c1
TruLem <6351071040@st.utc2.edu.vn>
2025-09-29 11:33:22 +0700

    feat: add healthcare images and enhance landing page layout
    - Added three new healthcare images to the public directory.
    - Updated global CSS to define color variables for better theming.
    - Modified page styles to adjust section heights and improve layout.
    - Introduced a new button component with customizable properties.
    - Created a card component to display healthcare information dynamically.
    - Enhanced the landing page with a grid section for displaying cards and a custom content block.

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,5 +1,29 @@
-const Card = () => {
-  return <div></div>;
+import React from 'react';
+
+import styles from './Card.module.scss';
+import FeedbackIcon from '../icons/feedback';
+
+interface CardProps {
+  id: number;
+  index: number;
+  description: string;
+  image: string;
+}
+
+const Card = ({ description, image, index }: CardProps) => {
+  return (
+    <div className={styles.card}>
+      {index !== 1 && (
+        <p className={styles.card_header_description}>{description}</p>
+      )}
+      <img src={image} alt={description} className={styles.card_image} />
+      {index !== 1 && (
+        <div
+          className={`${styles.overlay} ${index === 0 ? styles.custom_block : ''}`}
+        ></div>
+      )}
+    </div>
+  );
 };
 
 export default Card;
