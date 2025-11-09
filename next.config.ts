843092d5a6c5e3fdb6f861749c5d36100a15d732
congsinhv <6351071062@st.utc2.edu.vn>
2025-11-09 15:20:05 +0700

    feat: configure Next.js for standalone output
    - Updated next.config.ts to set the output to 'standalone', optimizing the build for deployment in serverless environments.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   webpack(config) {
     // SVG handling with SVGR
     config.module.rules.push({
