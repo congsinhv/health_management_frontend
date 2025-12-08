7fec14b052b3be16608abf694a22c9917e3e3504
congsinhv <6351071062@st.utc2.edu.vn>
2025-12-08 11:10:06 +0700

    feat(tests): integrate Jest for testing framework
    - Added Jest configuration file (jest.config.js) and setup file (jest.setup.js) for testing.
    - Updated package.json to include Jest commands for running tests and coverage.
    - Integrated Jest dependencies into the project to enhance testing capabilities.

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: [
+    '<rootDir>/src/components/profile/__tests__/DeviceList.test.tsx',
+  ],
+};
+
+module.exports = createJestConfig(customJestConfig);
